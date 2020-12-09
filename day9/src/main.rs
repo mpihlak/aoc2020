58529aab55cb0d92fc9875bf81201f6eab1578de
Martin Pihlak <martin.pihlak@starship.co>
2020-12-09 20:38:36 +0200

    Day9 part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -8,6 +8,7 @@ fn main() {
         .collect();
 
     let preamble = 25;
+    let mut invalid_number = None;
 
     for pos in preamble..numbers.len() {
         let window_start = pos - preamble;
@@ -25,7 +26,28 @@ fn main() {
 
         if !sum_found {
             println!("Stage 1: {} is not sum of previous {}", numbers[pos], preamble);
+            invalid_number = Some(numbers[pos]);
             break;
         }
     }
+
+    let need_this_sum = invalid_number.unwrap();
+'outer2:
+    for i in 0..numbers.len() {
+        let mut sum = numbers[i];
+        let mut min_number = numbers[i];
+        let mut max_number = numbers[i];
+        for num in numbers.iter().skip(i+1) {
+            sum += *num;
+            max_number = *num.max(&max_number);
+            min_number = *num.min(&min_number);
+            if sum == need_this_sum {
+                println!("Stage 2: sum of min and max = {}", min_number + max_number);
+                break 'outer2;
+            } else if sum > need_this_sum {
+                break;
+            }
+        }
+    }
+
 }
