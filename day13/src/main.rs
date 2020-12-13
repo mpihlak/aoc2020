ba201df9060639b22dac6c071e2a51df806cd8d6
Martin Pihlak <martin.pihlak@starship.co>
2020-12-13 10:57:28 +0200

    Brute forcing it to get some intuition

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -5,7 +5,8 @@ fn main() {
 
     let mut split = input_data.split('\n');
     let earliest_departure: u64 = split.next().unwrap().parse().unwrap();
-    let bus_list: Vec<u64> = split.next().unwrap()
+    let raw_bus_list = split.next().unwrap();
+    let bus_list: Vec<u64> = raw_bus_list
         .split(',')
         .filter(|x| *x != "x")
         .map(|x| x.parse().unwrap())
@@ -33,4 +34,33 @@ fn main() {
     println!("Earliest possible bus time = {}, Bus Id = {}", earliest_bus_time, earliest_bus_id);
     let answer = (earliest_bus_time - earliest_departure) * earliest_bus_id;
     println!("Stage 1: answer = {}", answer);
+
+    let bus_list: Vec<Option<u64>> = raw_bus_list
+        .split(',')
+        .map(|x| if x == "x" { None } else { Some(x.parse().unwrap()) })
+        .collect();
+    println!("Stage 2 list = {:?}", bus_list);
+
+    // period is at least min bus_id * max bus_id?
+    // t % min_bus_id == 0 && t+n % max_bus_id == 0
+    // maybe find the first occurrence of this and then get a gcd
+    // then step forward with the gcd as a step and validate?
+
+    // Brute force it
+    let mut t = 0;
+    loop {
+        let mut have_match = true;
+        for (pos, maybe_bus_id) in bus_list.iter().enumerate() {
+            if let Some(bus_id) = maybe_bus_id {
+                if (t + pos as u64) % bus_id != 0 {
+                    have_match = false;
+                    break;
+                }
+            }
+        }
+        if have_match {
+            println!("Found it at position {}", t);
+        }
+        t += 1;
+    }
 }
