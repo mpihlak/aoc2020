e63f3feff7e511977ac759ca8170513e5c965351
Martin Pihlak <martin.pihlak@starship.co>
2020-12-21 21:55:46 +0200

    Image extraction from sample data

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -3,6 +3,7 @@ use aoclib::*;
 #[derive(Debug,Clone)]
 struct Tile {
     sides: Vec<String>,
+    grid: Grid,
     tile_id: u64,
 }
 
@@ -31,38 +32,44 @@ impl Tile {
 
         Tile {
             tile_id,
+            grid,
             sides: vec![top, right, bottom, left],
         }
     }
 
     #[allow(dead_code)]
     fn to_str(&self) -> String {
-        let w = self.sides[0].len();
-        let h = w;
-        let mut res = String::new(); // format!("Tile {}\n", self.tile_id);
+        let w = self.grid.cells.len();
+        println!("w={}",w);
+        let mut res = String::new(); //format!("Tile {}\n", self.tile_id);
 
-        res.push_str(&self.sides[0]);
-        res.push('\n');
-
-        for i in 1..h-1 {
-            let c = self.sides[3].chars().nth(i).unwrap();
-            res.push(c);
-            res.push_str(&".".repeat(w-2));
-            let c = self.sides[1].chars().nth(i).unwrap();
-            res.push(c);
-            res.push('\n');
+        for row in 0..w {
+            let s: String = self.grid.cells[row].iter().collect();
+            res.push_str(&format!("{}\n", s));
         }
-
-        res.push_str(&self.sides[2]);
         res.push('\n');
         res
     }
 
 
     // Rotate clockwise
+    //  4 3 1    8 2 4
+    //  2 7 6 -> 9 7 3
+    //  8 9 0    0 6 1
     fn rotate(&self) -> Tile {
+        let mut rotated_grid = self.grid.clone();
+
+        assert_eq!(self.grid.width, self.grid.height);
+        let side_len = self.grid.width;
+        for i in 0..side_len {
+            for j in 0..side_len {
+                rotated_grid.cells[j][side_len-i-1] = self.grid.cells[i][j];
+            }
+        }
+
         Tile {
             tile_id: self.tile_id,
+            grid: rotated_grid,
             sides: vec![
                 self.sides[3].chars().rev().collect::<String>(),
                 self.sides[0].clone(),
@@ -71,29 +78,50 @@ impl Tile {
         }
     }
 
-    // Flip: 0 - nothiong, 1 - upside down, 2 - left right
+    // Flip: 0 - nothing, 1 - upside down, 2 - left right
+    //  4 3 1    8 9 0
+    //  2 7 6 -> 2 7 6
+    //  8 9 0    4 3 1
     fn flip(&self, how: usize) -> Tile {
-        let sides = if how == 1 {
-            vec![
-                self.sides[2].clone(),
-                self.sides[1].chars().rev().collect::<String>(),
-                self.sides[0].clone(),
-                self.sides[3].chars().rev().collect::<String>(),
-            ]
-        } else if how == 2 {
-            vec![
-                self.sides[0].chars().rev().collect::<String>(),
-                self.sides[3].clone(),
-                self.sides[2].chars().rev().collect::<String>(),
-                self.sides[1].clone(),
-            ]
-        } else {
-            self.sides.clone()
-        };
+        let mut flipped_grid = self.grid.clone();
+        let side_len = self.grid.width;
 
-        Tile {
-            tile_id: self.tile_id,
-            sides
+        if how == 1 {
+            for i in 0..side_len {
+                for j in 0..side_len {
+                    flipped_grid.cells[side_len-i-1][j] = self.grid.cells[i][j];
+                }
+            }
+
+            Tile {
+                tile_id: self.tile_id,
+                grid: flipped_grid,
+                sides: vec![
+                    self.sides[2].clone(),
+                    self.sides[1].chars().rev().collect::<String>(),
+                    self.sides[0].clone(),
+                    self.sides[3].chars().rev().collect::<String>(),
+                ],
+            }
+        } else if how == 2 {
+            for i in 0..side_len {
+                for j in 0..side_len {
+                    flipped_grid.cells[i][side_len-j-1] = self.grid.cells[i][j];
+                }
+            }
+
+            Tile {
+                tile_id: self.tile_id,
+                grid: flipped_grid,
+                sides: vec![
+                    self.sides[0].chars().rev().collect::<String>(),
+                    self.sides[3].clone(),
+                    self.sides[2].chars().rev().collect::<String>(),
+                    self.sides[1].clone(),
+                ]
+            }
+        } else {
+            self.clone()
         }
     }
 }
@@ -144,15 +172,16 @@ impl Puzzle {
         mut used: &mut Vec<bool>,
         used_count: usize,
         mut solution: &mut Vec<Vec<Option<Tile>>>,
-    ) -> Option<u64>
+    ) -> Option<Vec<Vec<Option<Tile>>>>
     {
         if used_count >= self.tiles.len() {
             let a = solution[0][0].as_ref().unwrap().tile_id;
             let b = solution[0][self.side_len-1].as_ref().unwrap().tile_id;
             let c = solution[self.side_len-1][0].as_ref().unwrap().tile_id;
             let d = solution[self.side_len-1][self.side_len-1].as_ref().unwrap().tile_id;
-            println!("Found a solution: [{}, {}, {}, {}]", a, b, c, d);
-            return Some(a*b*c*d);
+            let answer = a*b*c*d;
+            println!("Found a solution: {} [{}, {}, {}, {}]", answer, a, b, c, d);
+            return Some(solution.to_vec());
         }
 
         let row = pos / self.side_len;
@@ -192,8 +221,8 @@ impl Puzzle {
         None
     }
 
-    #[allow(dead_code)]
-    fn solve_by_bruteforce(&self, corners: &[u64]) -> Option<u64> {
+    // Knowing the corner pieces try to put together the whole image.
+    fn solve(&self, corners: &[u64]) -> Option<Vec<Vec<Option<Tile>>>> {
         let mut used: Vec<bool> = (0..self.tiles.len()).map(|_| false).collect();
         let mut solution = Vec::new();
         for _ in 0..self.side_len {
@@ -215,7 +244,6 @@ impl Puzzle {
             for how in 0..3 {
                 tile = tile.flip(how);
                 for _ in 0..4 {
-                    println!("Trying corner tile {:?}", tile);
                     solution[0][0] = Some(tile.clone());
                     if let Some(answer) = self.place_tile(1, &mut used, 1, &mut solution) {
                         return Some(answer);
@@ -230,6 +258,9 @@ impl Puzzle {
         None
     }
 
+    // Find the edges of the image. We know that the edges will not match any
+    // other tile, so this should give us 4 tiles that have 2 sides that don't
+    // have any matches to other tiles (no matter how rotated or flipped).
     fn find_corner_tiles(&self) -> Vec<u64> {
         let mut corners = Vec::new();
 
@@ -266,6 +297,71 @@ impl Puzzle {
         corners
     }
 
+    fn merge_to_tile(&self, image: &Vec<Vec<Option<Tile>>>) -> Tile {
+        let mut image_str = String::new();
+        let tile_side_len = image[0][0].as_ref().unwrap().grid.cells.len();
+
+        for row in 0..image.len() {
+            for tile_row in 1..tile_side_len-1 {
+                let mut image_line_str = String::new();
+
+                for col in 0..image[row].len() {
+                    let tile = image[row][col].as_ref().unwrap();
+
+                    for tile_col in 1..tile_side_len-1 {
+                        // XXX: Why are the rows reversed here?
+                        image_line_str.push(tile.grid.cells[tile_side_len - tile_row - 1][tile_col]);
+                    }
+                }
+
+                image_str.push_str(&image_line_str);
+                image_str.push('\n');
+            }
+        }
+
+        Tile {
+            tile_id: 0,
+            sides: vec![],
+            grid: Grid::from_str(&image_str),
+        }
+    }
+
+}
+
+#[allow(dead_code)]
+fn test_rotate() {
+    let mut tile = Tile::from_str(
+"Tile 1:
+##.#
+#...
+#.##
+.#..");
+    println!("Original\n{}", tile.to_str());
+    for i in 0..4 {
+        tile = tile.rotate();
+        println!("Rotate {}\n{}", i, tile.to_str());
+    }
+}
+
+#[allow(dead_code)]
+fn test_flip() {
+    let mut tile = Tile::from_str(
+"Tile 1:
+##.#
+#...
+#.##
+.#..");
+    println!("Original\n{}", tile.to_str());
+    for i in 0..3 {
+        tile = tile.flip(i);
+        println!("Flip {}\n{}", i, tile.to_str());
+    }
+}
+
+// Find the sea monsters and count the '#' characters that are not
+// part of the monster.
+fn calculate_roughness(image_tile: &Tile) -> u32 {
+    0
 }
 
 fn main() {
@@ -280,6 +376,11 @@ fn main() {
     let answer: u64 = corners.iter().product();
     println!("Stage 1: answer = {:?}", answer);
 
-    let answer = puzzle.solve_by_bruteforce(&corners);
-    println!("Stage 1: bruteforced answer = {:?}", answer);
+    let unprocessed_image = puzzle.solve(&corners).unwrap();
+    println!("Removing edges");
+    let image_tile = puzzle.merge_to_tile(&unprocessed_image);
+    println!("Merged image:\n{}", image_tile.to_str());
+
+    let answer = calculate_roughness(&image_tile);
+    println!("Stage 2: answer = {}", answer);
 }
