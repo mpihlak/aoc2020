e846d9ff174c8cd2d3e541fc9cb1974152b8fc54
Martin Pihlak <martin.pihlak@starship.co>
2020-12-22 21:51:54 +0200

    Tidy up

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,31 +1,20 @@
 use aoclib::*;
 use std::collections::HashSet;
 
-fn play_combat(player1: &Vec<usize>, player2: &Vec<usize>) -> Vec<usize> {
-    let mut player1 = player1.clone();
-    let mut player2 = player2.clone();
+fn play_combat(player1: &[usize], player2: &[usize]) -> Vec<usize> {
+    let mut player1 = player1.to_owned();
+    let mut player2 = player2.to_owned();
 
     while !player1.is_empty() && !player2.is_empty() {
-        //println!("Round {}", round);
-        //println!("Player 1 deck: {:?}", player1);
-        //println!("Player 2 deck: {:?}", player2);
-
         let card1 = player1.remove(0);
         let card2 = player2.remove(0);
         
-        //println!("Player 1 plays {}", card1);
-        //println!("Player 2 plays {}", card2);
-
         if card1 > card2 {
-            //println!("Player 1 wins the round");
             player1.push(card1);
             player1.push(card2);
-        } else if card2 > card1 {
-            //println!("Player 2 wins the round");
+        } else {
             player2.push(card2);
             player2.push(card1);
-        } else {
-            panic!("Never happens!");
         }
     }
 
@@ -36,7 +25,7 @@ fn play_combat(player1: &Vec<usize>, player2: &Vec<usize>) -> Vec<usize> {
     }
 }
 
-fn key(hand1: &Vec<usize>, hand2: &Vec<usize>) -> u128 {
+fn key(hand1: &[usize], hand2: &[usize]) -> u128 {
     let u1: u128 = hand1.iter()
         .map(|x| 1 << *x)
         .fold(0, |acc, x| acc | x);
@@ -48,10 +37,10 @@ fn key(hand1: &Vec<usize>, hand2: &Vec<usize>) -> u128 {
     u1 << 64 | u2
 }
 
-fn play_recursive_combat(player1: &Vec<usize>, player2: &Vec<usize>) -> (Vec<usize>, bool) {
+fn play_recursive_combat(player1: &[usize], player2: &[usize]) -> (Vec<usize>, bool) {
     let mut played_hands = HashSet::new();
-    let mut player1 = player1.clone();
-    let mut player2 = player2.clone();
+    let mut player1 = player1.to_owned();
+    let mut player2 = player2.to_owned();
 
     while !player1.is_empty() && !player2.is_empty() {
         if played_hands.contains(&key(&player1, &player2)) {
@@ -60,16 +49,9 @@ fn play_recursive_combat(player1: &Vec<usize>, player2: &Vec<usize>) -> (Vec<usi
 
         played_hands.insert(key(&player1, &player2));
 
-        //println!("Round {}", round);
-        //println!("Player 1 deck: {:?}", player1);
-        //println!("Player 2 deck: {:?}", player2);
-
         let card1 = player1.remove(0);
         let card2 = player2.remove(0);
 
-        //println!("Player 1 plays {}", card1);
-        //println!("Player 2 plays {}", card2);
-        
         let player1_won = if player1.len() >= card1 && player2.len() >= card2 {
             let (_hand, was_it_player1) = play_recursive_combat(
                 &(&player1[..card1]).to_vec(),
@@ -112,16 +94,12 @@ fn main() {
         .collect();
 
     let result = play_combat(&player1, &player2);
-    println!("Play results = {:?}", result);
-
     let answer: u64 = result.iter().enumerate()
         .map(|(pos, val)| (result.len() - pos) as u64 * *val as u64)
         .sum();
     println!("Part 1 answer = {}", answer);
 
     let (result, _winner) = play_recursive_combat(&player1, &player2);
-    println!("Play results = {:?}", result);
-
     let answer: u64 = result.iter().enumerate()
         .map(|(pos, val)| (result.len() - pos) as u64 * *val as u64)
         .sum();
