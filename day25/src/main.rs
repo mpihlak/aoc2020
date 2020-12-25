a85624197670995a1635e10b5dab9d9996570ad6
Martin Pihlak <martin.pihlak@starship.co>
2020-12-25 22:41:19 +0200

    day25

---
+++ day25/src/main.rs
@@ -0,0 +1,46 @@
+fn transform(value: u64, subject_number: u64) -> u64 {
+    (value * subject_number) % 20201227
+}
+
+fn transform_loop(subject_number: u64, loop_size: usize) -> u64 {
+    let mut val = 1;
+
+    for _ in 0..loop_size {
+        val = transform(val, subject_number);
+    }
+
+    return val;
+}
+
+fn find_loop_size(public_key: u64) -> usize {
+    let mut val = 1;
+    let mut loop_size = 0;
+    loop {
+        loop_size += 1;
+        val = transform(val, 7);
+        if val == public_key {
+            return loop_size;
+        }
+    }
+}
+
+fn main() {
+    let door_public_key = 17807724;
+    let card_public_key = 5764801;
+    let door_loop_size = find_loop_size(door_public_key);
+    let card_loop_size = find_loop_size(card_public_key);
+    println!("Door loop size = {}", door_loop_size);
+    println!("Card loop size = {}", card_loop_size);
+    println!("Encryption key = {}", transform_loop(door_public_key, card_loop_size));
+    println!("Encryption key = {}", transform_loop(card_public_key, door_loop_size));
+    println!();
+
+    let door_public_key = 2084668;
+    let card_public_key = 3704642;
+    let door_loop_size = find_loop_size(door_public_key);
+    let card_loop_size = find_loop_size(card_public_key);
+    println!("Door loop size = {}", door_loop_size);
+    println!("Card loop size = {}", card_loop_size);
+    println!("Encryption key = {}", transform_loop(door_public_key, card_loop_size));
+    println!("Encryption key = {}", transform_loop(card_public_key, door_loop_size));
+}
