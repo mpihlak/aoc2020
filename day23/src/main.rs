aa6f137148fd741ac8079959b8d3ea7ac7738b84
Martin Pihlak <martin.pihlak@starship.co>
2020-12-25 22:40:50 +0200

    Fumbling around

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -4,6 +4,7 @@ const MIN_CUP: usize = 1;
 fn one_move(cups: &[usize], current_pos: usize) -> Vec<usize> {
     let mut result = Vec::new();
 
+    let _current_pos = current_pos;
     let current_pos = current_pos % cups.len();
     let current_cup = cups[current_pos];
 
@@ -12,6 +13,10 @@ fn one_move(cups: &[usize], current_pos: usize) -> Vec<usize> {
         cups[(current_pos+2) % cups.len()],
         cups[(current_pos+3) % cups.len()],
     ];
+
+    if selected.contains(&1) {
+        println!("{:05}: 1 selected", _current_pos);
+    }
 
     let mut dst_cup = cups[current_pos];
     loop {
@@ -24,13 +29,6 @@ fn one_move(cups: &[usize], current_pos: usize) -> Vec<usize> {
             break;
         }
     }
-
-    /*
-    println!("cups: {:?}", cups);
-    println!("current cup: {}", cups[current_pos]);
-    println!("picked up: {:?}", selected);
-    println!("next cup id: {}", dst_cup);
-    */
 
     let mut i = 0;
     while i < cups.len() {
@@ -55,6 +53,7 @@ fn one_move(cups: &[usize], current_pos: usize) -> Vec<usize> {
     result
 }
 
+#[allow(dead_code)]
 fn cup_labels(cups: &[usize]) -> String {
     let one_pos = cups.iter()
         .enumerate()
@@ -71,15 +70,56 @@ fn cup_labels(cups: &[usize]) -> String {
     result
 }
 
+#[allow(dead_code)]
+fn diff(a: &[usize], b: &[usize]) -> usize {
+    let mut diffs = 0;
+    for i in 0..a.len() {
+        if a[i] != b[i] {
+            diffs += 1;
+        }
+    }
+    diffs
+}
+
 fn main() {
-    let input_data = vec![4, 7, 6, 1, 3, 8, 2, 5, 9];       // actual input
-    let _input_data = vec![3, 8, 9, 1, 2, 5, 4, 6, 7];       // sample
+    let _input_data = vec![4, 7, 6, 1, 3, 8, 2, 5, 9];       // actual input
+    let input_data = vec![3, 8, 9, 1, 2, 5, 4, 6, 7];       // sample
 
     let mut cups = input_data.to_owned();
     for nth_move in 0..100 {
         cups = one_move(&cups, nth_move);
     }
 
-    println!("Final cups = {:?}", cups);
-    println!("Labels = {}", cup_labels(&cups));
+    println!("Part1 answer, final cups = {:?}", cups);
+    println!();
+
+    let mut cups = input_data.to_owned();
+    let padding: Vec<usize> = (10..101).collect();
+    cups.extend(padding);
+
+    let mut counts = std::collections::HashMap::new();
+
+    //println!("Part2 initial cups       = {:?}", cups);
+    println!("Part2 cup count = {}", cups.len());
+    for nth_move in 0..10000 {
+        cups = one_move(&cups, nth_move);
+
+        let pos = cups.iter().position(|x| *x == 1).unwrap();
+        let v = vec![
+            cups[pos],
+            cups[(pos+1) % cups.len()],
+            cups[(pos+2) % cups.len()],
+        ];
+
+        let entry = counts.entry(v.clone()).or_insert(0);
+        *entry += 1;
+    }
+
+    println!("Part2 answer, final cups = {:?}", cups);
+
+    /*
+    for (k, v) in counts.iter() {
+        println!("{:?}: {}", k, v);
+    }
+    */
 }
