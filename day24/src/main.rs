774f0292ce549dd8e89bdac42668b7be613564d3
Martin Pihlak <martin.pihlak@starship.co>
2021-01-02 00:28:53 +0200

    Use a faster approach

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -49,19 +49,42 @@ impl Tiling {
 
     fn next_iteration(&mut self) {
         let mut flips = Vec::new();
+        let mut visited = std::collections::HashSet::new();
 
-        // TODO: Really ought to track the bounding box of the tiles
-        // so that we're not doing unnecessary work here.
-        let mut offset = 1;
-        for row in (2..self.grid.len()-2).step_by(2) {
-            for col in (2+offset..self.grid[0].len()-2).step_by(2) {
-                if self.needs_to_flip(row, col) {
-                    flips.push((row, col));
+        // Now, this would be a lot cheaper if the black cells were kept in a HashMap
+        let mut cells_to_visit = Vec::new();
+        for (row_num, row) in self.grid.iter().enumerate() {
+            for (col_num, x) in row.iter().copied().enumerate() {
+                if x == 1 {
+                    cells_to_visit.push((row_num, col_num));
+                    visited.insert((row_num, col_num));
                 }
             }
-            offset = (offset + 1) % 2;
         }
 
+        while let Some((row, col)) = cells_to_visit.pop() {
+            visited.insert((row, col));
+
+            // Visit only the black tiles, because the only flips are going to be 
+            // the black tiles themselves or their white neighbors.
+            if self.grid[row][col] == 1 {
+                for direction in vec!["e", "se", "sw", "w", "nw", "ne"] {
+                    let (r, c) = adjust(direction, row, col);
+                    if self.grid[row][col] == 1 && !visited.contains(&(r, c)) {
+                        // odd distances from the middle row get an offset
+                        let middle = self.grid.len() / 2;
+                        let offset = ((r as i32 - middle as i32).abs() % 2) as usize;
+                        cells_to_visit.push((r, c + offset));
+                    }
+                }
+            }
+
+            if self.needs_to_flip(row, col) {
+                flips.push((row, col));
+            }
+        }
+
+        // Now, apply the flips
         for (row, col) in flips.iter().copied() {
             self.grid[row][col] = (self.grid[row][col] + 1) % 2;
         }
@@ -138,15 +161,11 @@ fn main() {
 
     // Represent the hex grid in a 2d array, with extra blank space
     // for the 6 sides. Start with the reference tile at the middle
-    // of the grid.
-    let grid_width = num_tiles*8;
+    // of the grid. Allocate generously so that we don't run outside of the grid.
+    let grid_width = num_tiles*10;
     let grid_height = grid_width;
-    let start_tile_row = num_tiles*4;
+    let start_tile_row = grid_width/2;
     let start_tile_col = start_tile_row;
-    let mut min_row = start_tile_row;
-    let mut max_row = start_tile_row;
-    let mut min_col = start_tile_col;
-    let mut max_col = start_tile_col;
     let mut grid: Vec<Vec<i32>> = (0..grid_height).map(|_| vec![0; grid_width]).collect();
 
     println!("Grid len = {}", grid.len());
@@ -167,17 +186,12 @@ fn main() {
         } else {
             grid[row][col] = 0;
         }
-
-        min_row = min_row.min(row);
-        min_col = min_col.min(col);
-        max_row = max_row.max(row);
-        max_col = max_col.max(col);
     }
 
     let num_black_tiles: i32 = grid.iter().flatten().sum();
     println!("Part 1: Number of black tiles = {}", num_black_tiles);
-
     let mut tiling = Tiling::new(grid);
+
     for _i in 0..100 {
         tiling.next_iteration();
     }
