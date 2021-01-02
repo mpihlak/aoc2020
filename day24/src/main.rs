b831b3c4412f35653d2c637f793e9a4217e7b02b
Martin Pihlak <martin.pihlak@starship.co>
2021-01-02 18:11:39 +0200

    Make it much faster

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,6 +1,7 @@
 use aoclib::*;
+use std::collections::{HashMap, HashSet};
 
-fn adjust(instruction: &str, row: usize, col: usize) -> (usize,usize) {
+fn adjust(instruction: &str, row: i32, col: i32) -> (i32,i32) {
     match instruction.as_ref() {
         "se" => ( row +  2, col +  1 ),
         "sw" => ( row +  2, col -  1 ),
@@ -13,29 +14,31 @@ fn adjust(instruction: &str, row: usize, col: usize) -> (usize,usize) {
 }
 
 struct Tiling {
-    grid: Vec<Vec<i32>>,
+    grid: HashMap<(i32,i32), i32>,
 }
 
 impl Tiling {
-    fn new(grid: Vec<Vec<i32>>) -> Self {
+    fn new(grid: HashMap<(i32,i32), i32>) -> Self {
         Tiling {
             grid,
         }
     }
 
-    fn count_neighbors(&self, row: usize, col: usize) -> i32 {
+    fn count_neighbors(&self, row: i32, col: i32) -> i32 {
         let mut neighbors = 0;
         for direction in vec!["e", "se", "sw", "w", "nw", "ne"] {
             let (r, c) = adjust(direction, row, col);
-            neighbors += self.grid[r][c];
+            if let Some(cell_value) = self.grid.get(&(r, c)) {
+                neighbors += cell_value;
+            }
         }
         neighbors
     }
 
-    fn needs_to_flip(&self, row: usize, col: usize) -> bool {
+    fn needs_to_flip(&self, tile_color: i32, row: i32, col: i32) -> bool {
         let n = self.count_neighbors(row, col);
 
-        if self.grid[row][col] == 1 {
+        if tile_color == 1 {
             if n == 0 || n > 2 {
                 return true;
             }
@@ -48,86 +51,88 @@ impl Tiling {
     }
 
     fn next_iteration(&mut self) {
+        // Create a list of all the black cells in the grid
+        let mut cells_to_visit: Vec<(i32,i32)> = self.grid.iter()
+            .filter(|(_key, val)| **val == 1)
+            .map(|(key, _val)| *key)
+            .collect();
+        let mut visited: HashSet<(i32,i32)> = cells_to_visit.iter().copied().collect();
+
         let mut flips = Vec::new();
-        let mut visited = std::collections::HashSet::new();
-
-        // Now, this would be a lot cheaper if the black cells were kept in a HashMap
-        let mut cells_to_visit = Vec::new();
-        for (row_num, row) in self.grid.iter().enumerate() {
-            for (col_num, x) in row.iter().copied().enumerate() {
-                if x == 1 {
-                    cells_to_visit.push((row_num, col_num));
-                    visited.insert((row_num, col_num));
-                }
-            }
-        }
-
         while let Some((row, col)) = cells_to_visit.pop() {
             visited.insert((row, col));
 
+            let cell = *self.grid.get(&(row, col)).unwrap_or(&0);
+
             // Visit only the black tiles, because the only flips are going to be 
             // the black tiles themselves or their white neighbors.
-            if self.grid[row][col] == 1 {
+
+            if cell == 1 {
                 for direction in vec!["e", "se", "sw", "w", "nw", "ne"] {
                     let (r, c) = adjust(direction, row, col);
-                    if self.grid[row][col] == 1 && !visited.contains(&(r, c)) {
-                        // odd distances from the middle row get an offset
-                        let middle = self.grid.len() / 2;
-                        let offset = ((r as i32 - middle as i32).abs() % 2) as usize;
-                        cells_to_visit.push((r, c + offset));
+                    if !visited.contains(&(r, c)) {
+                        cells_to_visit.push((r, c));
                     }
                 }
             }
 
-            if self.needs_to_flip(row, col) {
+            if self.needs_to_flip(cell, row, col) {
                 flips.push((row, col));
             }
         }
 
         // Now, apply the flips
         for (row, col) in flips.iter().copied() {
-            self.grid[row][col] = (self.grid[row][col] + 1) % 2;
+            let cell = self.grid.entry((row, col)).or_insert(0);
+            *cell = (*cell + 1) % 2;
         }
     }
 
     fn count_blacks(&self) -> i32 {
-        self.grid.iter().flatten().sum()
+        self.grid.values().sum()
     }
 
     #[allow(dead_code)]
     fn display(&self) {
-        let mut min_row = 999;
-        let mut max_row = 0;
-        let mut min_col = 999;
-        let mut max_col = 0;
 
-        let mut display_grid: Vec<Vec<char>> = self.grid.iter()
-            .map(|row| row.iter().map(|_| '.').collect()).collect();
+        // 1000x1000 should be enough for everyone
+        let width  = 1000;
+        let height = 1000;
+        let mid_row = height / 2;
+        let mid_col = width / 2;
 
-        for r in 0..self.grid.len() {
-            for c in 0..self.grid[r].len() {
-                if self.grid[r][c] == 1 {
-                    min_row = min_row.min(r-1);
-                    max_row = max_row.max(r+1);
-                    min_col = min_col.min(c);
-                    max_col = max_col.max(c+1);
+        let mut display_grid: Vec<Vec<char>> =
+            (0..height).map(|_|
+                (0..width).map(|_| '.').collect()
+            ).collect();
 
-                    display_grid[r-1][c] = '/';
-                    display_grid[r-1][c+1] = '\\';
-                    display_grid[r][c] = '#';
-                    display_grid[r][c+1] = '|';
-                    display_grid[r+1][c] = '\\';
-                    display_grid[r+1][c+1] = '/'
-                }
+        let mut min_row = std::usize::MAX;
+        let mut min_col = std::usize::MAX;
+        let mut max_row = std::usize::MIN;
+        let mut max_col = std::usize::MIN;
+
+        for ((r, c), v) in self.grid.iter() {
+            let r = ((mid_row as i32) + r) as usize;
+            let c = ((mid_col as i32) + c) as usize;
+
+            if *v == 1 {
+                display_grid[r-1][c] = '/';
+                display_grid[r-1][c+1] = '\\';
+                display_grid[r][c] = '#';
+                display_grid[r][c+1] = '|';
+                display_grid[r+1][c] = '\\';
+                display_grid[r+1][c+1] = '/';
+
+                min_row = min_row.min(r);
+                max_row = max_row.max(r);
+                min_col = min_col.min(c);
+                max_col = max_col.max(c);
             }
         }
 
-        println!("Min row = {}, max row = {}", min_row, max_row);
-        println!("Min col = {}, max col = {}", min_col, max_col);
-
-        for r in min_row..max_row+1 {
+        for r in min_row-1..max_row+2 {
             print!("{:03}: ", r);
-            for c in min_col..max_col+1 {
+            for c in min_col..max_col+2 {
                 print!("{}", display_grid[r][c]);
             }
             println!();
@@ -159,18 +164,22 @@ fn main() {
     let num_tiles = tile_instructions.len();
     println!("num tiles = {}", num_tiles);
 
-    // Represent the hex grid in a 2d array, with extra blank space
-    // for the 6 sides. Start with the reference tile at the middle
-    // of the grid. Allocate generously so that we don't run outside of the grid.
-    let grid_width = num_tiles*10;
-    let grid_height = grid_width;
-    let start_tile_row = grid_width/2;
-    let start_tile_col = start_tile_row;
-    let mut grid: Vec<Vec<i32>> = (0..grid_height).map(|_| vec![0; grid_width]).collect();
+    // Think about the hex grid as a 2d array, with extra space allocated
+    // for the 6 sides. Like this:
+    // ..........
+    // ./\/\.....
+    // .#|#|.....
+    // /\/\/\../\
+    // #|..#|..#|
+    // \/\.\/..\/
+    // .#|.......
+    // .\/.......
+    // ..........
 
-    println!("Grid len = {}", grid.len());
-    println!("Init pos = {}", start_tile_row);
+    let start_tile_row = 0i32;
+    let start_tile_col = 0i32;
 
+    let mut grid = HashMap::new();
     for tile_instr in tile_instructions.iter() {
         let mut row = start_tile_row;
         let mut col = start_tile_col;
@@ -181,16 +190,15 @@ fn main() {
             col = c;
         }
 
-        if grid[row][col] == 0 {
-            grid[row][col] = 1;
-        } else {
-            grid[row][col] = 0;
-        }
+        let entry = grid.entry((row, col)).or_insert(0);
+        *entry = (*entry + 1) % 2;
     }
 
-    let num_black_tiles: i32 = grid.iter().flatten().sum();
+    let num_black_tiles: i32 = grid.values().sum();
     println!("Part 1: Number of black tiles = {}", num_black_tiles);
+
     let mut tiling = Tiling::new(grid);
+    tiling.display();
 
     for _i in 0..100 {
         tiling.next_iteration();
